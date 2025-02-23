f17228328181dbe5c6e20a583c7c50c9c132daae
muhammedfarseent <farseentaneem@gmail.com>
2025-02-23 14:43:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let name = document.getElementById("name")
+let email = document.getElementById("email")
+let pass = document.getElementById("message")
+
+function sendMail(){
+    alert("Your Email sent Successfully")
+}
